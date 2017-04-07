049642d9f1db5c0a98261fcf36be1a1d64c83512
Woratham Kh <kabutoboy@gmail.com>
2017-04-07 20:10:05 +0700

    use map

--- set-1/challenge-3/solve.py
+++ set-1/challenge-3/solve.py
@@ -2,7 +2,5 @@ from itertools import chain
 
 C = bytearray.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
 for k in chain(range(ord('a'), ord('z')), range(ord('A'), ord('Z'))):
-    M = bytearray()
-    for c in C:
-        M.append(c ^ k)
+    M = bytearray(map(lambda c: c ^ k, C))
     print(chr(k), M.decode())
